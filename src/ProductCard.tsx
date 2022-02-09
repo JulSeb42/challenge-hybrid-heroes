7e9eca009b52d16dd3d6a4d861b5ad9d361fd544
Julien Sebag <julien.sebag@me.com>
2022-02-09 16:37:42 +0100

    Initial commit

---
+++ src/ProductCard.tsx
@@ -0,0 +1,28 @@
+import React, { useEffect } from "react"
+import { RefreshControl, ScrollView, StyleSheet, View, Text } from "react-native"
+
+export default function ProductCard({ product }) {
+    return <View style={styles.card}>
+
+    </View>
+}
+
+const variables = {
+    colors: {
+        cardBackground: "#F8F9FC",
+        text: "#1B2633",
+        darkGray: "#333333",
+        lightBlue: "#D5E6FF",
+        white: "#FFFFFF",
+    },
+
+    shadow: "0px 0px 3px 0px hsla(212, 31%, 15%, 0,25)",
+
+    
+}
+
+const styles = StyleSheet.create({
+    card: {
+
+    }
+})
